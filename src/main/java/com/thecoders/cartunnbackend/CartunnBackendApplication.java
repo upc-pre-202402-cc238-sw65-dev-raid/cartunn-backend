e676a63966b094bf3b2233538d02062b91a9a0c2
NickDavid1811 <106049371+NickDavid1811@users.noreply.github.com>
2024-05-28 09:08:25 -0500

    chore: initial structure for Cartunn backend

---
+++ src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thecoders.cartunnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartunnBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartunnBackendApplication.class, args);
+    }
+
+}
