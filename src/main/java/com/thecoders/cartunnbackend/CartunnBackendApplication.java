7c03ee96fdcd1a7f50840f2c25e8b1bd15cb28b1
MarianaAlexandra05 <u202217389@upc.edu.pe>
2024-06-02 00:09:34 -0500

    feat(shared): add AuditableAbstractAggregateRoot and AuditableModel

--- src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
+++ src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
@@ -2,8 +2,11 @@ package com.thecoders.cartunnbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+
 public class CartunnBackendApplication {
 
     public static void main(String[] args) {
