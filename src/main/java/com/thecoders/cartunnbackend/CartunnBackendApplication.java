cd3c9415b439e2a2941272e2d78e91f7ade69439
NickDavid1811 <u20201c607@upc.edu.pe>
2024-06-04 09:09:15 -0500

    feat: update de aplication properties and pom.xml

--- src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
+++ src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
@@ -2,7 +2,11 @@ package com.thecoders.cartunnbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -11,6 +15,19 @@ public class CartunnBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CartunnBackendApplication.class, args);
+    }
+
+    @Configuration
+    public static class Myconfiguration{
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**").allowedMethods("HEAD","GET", "POST", "PUT", "DELETE","PATCH");
+                }
+            };
+        }
     }
 
 }
