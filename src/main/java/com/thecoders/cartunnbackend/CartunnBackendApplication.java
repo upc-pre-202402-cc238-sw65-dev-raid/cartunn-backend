8a5cb6ebd56472816c32f6b91261f09a81d70a59
abrancitouu <abrahamayquipa2000@gmail.com>
2024-06-19 15:32:47 -0500

    fix: allow the cors origin

--- src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
+++ src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
@@ -25,7 +25,7 @@ public class CartunnBackendApplication {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOrigins("*")
+                            .allowedOrigins("https://cartunn-frontend.netlify.app", "http://localhost:4200")
                             .allowedMethods("HEAD","GET", "POST", "PUT", "DELETE","PATCH")
                             .allowedHeaders("*")
                             .allowCredentials(true);
