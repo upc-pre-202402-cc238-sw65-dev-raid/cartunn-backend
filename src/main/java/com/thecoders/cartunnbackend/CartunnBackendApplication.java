f26056b902f9930a157e116d736ce0e65a0d0b6d
Natanael David <106049371+NickDavid1811@users.noreply.github.com>
2024-06-24 21:05:54 -0500

    chore: Update CartunnBackendApplication.java

--- src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
+++ src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
@@ -11,27 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @EnableJpaAuditing
 public class CartunnBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CartunnBackendApplication.class, args);
     }
-
-    @Configuration
-    public static class Myconfiguration {
-        @Bean
-        public WebMvcConfigurer corsConfigurer() {
-            return new WebMvcConfigurer() {
-                @Override
-                public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**")
-                            .allowedOrigins("https://cartunn-frontend.netlify.app/**", "http://localhost:4200/**", "https://cartunnbackend.up.railway.app/swagger-ui/index.html/**")
-                            .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
-                            .allowedHeaders("*")
-                            .allowCredentials(true)
-                    ;
-                }
-            };
-        }
-    }
-
 }
