a482dd8c2de47e413070a43d454470df073e8c4d
NickDavid1811 <u20201c607@upc.edu.pe>
2024-11-14 23:03:04 -0500

    fix: added iam and purchasing

--- src/main/java/com/thecoders/cartunnbackend/purchasing/interfaces/rest/resources/OrderResource.java
+++ src/main/java/com/thecoders/cartunnbackend/purchasing/interfaces/rest/resources/OrderResource.java
@@ -2,5 +2,6 @@ package com.thecoders.cartunnbackend.purchasing.interfaces.rest.resources;
 
 import java.time.LocalDate;
 
-public record OrderResource(Long id, String name, String description, int code, LocalDate entryDate, LocalDate exitDate, String status) {
+public record OrderResource(Long id, String name, String description, int code, LocalDate entryDate, LocalDate exitDate,
+                            String status, String imageUrl) {
 }
--- src/main/java/com/thecoders/cartunnbackend/purchasing/interfaces/rest/resources/UpdateOrderResource.java
+++ src/main/java/com/thecoders/cartunnbackend/purchasing/interfaces/rest/resources/UpdateOrderResource.java
@@ -2,5 +2,6 @@ package com.thecoders.cartunnbackend.purchasing.interfaces.rest.resources;
 
 import java.time.LocalDate;
 
-public record UpdateOrderResource(String name, String description, int code, LocalDate entryDate, LocalDate exitDate, String status) {
+public record UpdateOrderResource(String name, String description, int code, LocalDate entryDate, LocalDate exitDate,
+                                  String status, String imageUrl) {
 }
